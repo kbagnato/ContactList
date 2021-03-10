89118b2072c1e8e43f681217f611bc07c3586610
Kevin Bagnato <kevinbagnato@gmail.com>
2021-03-10 10:18:23 -0500

    change db to mongo atlas

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-var MongoClient = require("mongodb").MongoClient;
+// var MongoClient = require("mongodb").MongoClient;
 var session = require('express-session');
 var bcrypt = require("bcryptjs");
 
@@ -28,28 +28,43 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-// setup mongoDB connection
-var people;
-var url = "mongodb://localhost:27017/test";
-MongoClient.connect(url, function(err, db) {
-	if (err) throw err;
 
-	// mongoDB is test.people
-	var dbo = db.db("test");
-	people = dbo.collection("people");
+
+// const uri = process.env.MONGODB_URI;
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://kevinb:h-RFm7PEvnK_8in@contacts-cluster.0zhzv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("contactsDb").collection("people");
+  // perform actions on the collection object
+  client.close();
 });
 
-// attach db people to each request 
-app.use(function(req, res, next) {
-	req.people = people;
-	next();
-});
+
+// // setup mongoDB connection
+// var people;
+// var url = "mongodb://localhost:27017/test";
+// MongoClient.connect(url, function(err, db) {
+// 	if (err) throw err;
+
+// 	// mongoDB is test.people
+// 	var dbo = db.db("test");
+// 	people = dbo.collection("people");
+// });
+
+// // attach db people to each request 
+// app.use(function(req, res, next) {
+// 	req.people = people;
+// 	next();
+// });
 
 // setup singleton username/password on server
 var username = "cmps369";
 var password = "finalproject";
-bcrypt.genSalt(10, function(err, salt) {
-	bcrypt.hash(password, salt, function(err, hash) {
+bcrypt.genSalt(10, function (err, salt) {
+	bcrypt.hash(password, salt, function (err, hash) {
 		password = hash;
 		console.log("Hashed password: " + password);
 	});
@@ -58,7 +73,7 @@ bcrypt.genSalt(10, function(err, salt) {
 // setup passport for authentication
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-app.use(session({ secret: 'cmps369'}))
+app.use(session({ secret: 'cmps369' }))
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -67,7 +82,7 @@ passport.use(new LocalStrategy(
 		usernameField: 'username',
 		passwordField: 'password'
 	},
-	function(user, pswd, done) {
+	function (user, pswd, done) {
 		// look up user in database
 		if (user != username) {
 			console.log("Username not found");
@@ -75,7 +90,7 @@ passport.use(new LocalStrategy(
 		}
 
 		// check credentials
-		bcrypt.compare(pswd, password, function(err, isMatch) {
+		bcrypt.compare(pswd, password, function (err, isMatch) {
 			if (err) return done(err);
 			if (isMatch) {
 				console.log("Login successfull.");
@@ -88,28 +103,28 @@ passport.use(new LocalStrategy(
 	}
 ));
 
-passport.serializeUser(function(username, done) {
+passport.serializeUser(function (username, done) {
 	// this is called when the user object associated with the session
 	// needs to be turned into a string.  Since we are only storing the user
 	// as a string - just return the username.
 	done(null, username);
 });
 
-passport.deserializeUser(function(username, done) {
+passport.deserializeUser(function (username, done) {
 	// normally we would find the user in the database and
 	// return an object representing the user (for example, an object
 	// that also includes first and last name, email, etc)
 	done(null, username);
- });
+});
 
 
 // Posts to login will have username/password form data.
 // passport will call the appropriate functions...
-indexRouter.post('/login', passport.authenticate('local', { successRedirect: '/contacts', failureRedirect: '/login_fail',})
+indexRouter.post('/login', passport.authenticate('local', { successRedirect: '/contacts', failureRedirect: '/login_fail', })
 );
 
 indexRouter.get('/login', function (req, res) {
-	res.render('login', {title: 'Login'});
+	res.render('login', { title: 'Login' });
 });
 
 indexRouter.get('/login_fail', function (req, res) {
@@ -128,19 +143,19 @@ app.use('/', indexRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
